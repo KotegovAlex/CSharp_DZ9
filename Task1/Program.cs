da0a79da1752775b58c346b37a871da96d88f19d
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-07-19 18:20:30 +0500

    Added Task1 even 2 numbers from M to N

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+
+int Promt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintEvenTwoNaturalList(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    PrintEvenTwoNaturalList(M, N - 1);
+    if (N % 2 == 0) Console.Write(N + " ");
+}
+
+int M = Promt("Введите натуральное число M > ");
+int N = Promt("Введите натуральное число N > ");
+PrintEvenTwoNaturalList(M, N);
