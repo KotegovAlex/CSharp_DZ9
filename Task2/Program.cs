fd729d382591e0a23b6e21ed453ed7d180f8b747
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-07-19 18:30:45 +0500

    Added Task2 sum of natural numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, которая найдёт 
+сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30 */
+
+int Promt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalNumbersSum(int M, int N)
+{
+    if (N < M)
+    {
+        return 0;
+    }
+    return NaturalNumbersSum(M, N - 1) + N;
+}
+
+int M = Promt("Введите натуральное число M > ");
+int N = Promt("Введите натуральное число N > ");
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна: {NaturalNumbersSum(M, N)}");
