6611948a725058dcd3d907b9312d222cf327274f
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-07-19 19:11:09 +0500

    Added Task3 Ackermann Function

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 3: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int Promt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int AckermanFunction(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (M > 0 && N == 0) return AckermanFunction(M - 1, 1);
+    else if (M > 0 && N > 0) return AckermanFunction(M - 1, AckermanFunction(M, N - 1));
+    return 0;
+}
+
+int M = Promt("Введите неотрицательное число M > ");
+int N = Promt("Введите неотрицательное число N > ");
+
+if (M < 0 || N < 0)
+{
+    Console.WriteLine("Введите неотрицательные числа!!!");
+}
+else
+{
+    Console.WriteLine($"Значение функции Аккермана - А({M}, {N}) равно: {AckermanFunction(M, N)}");
+}
